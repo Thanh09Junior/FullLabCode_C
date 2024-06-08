7c7011eff56b1e07b910441b45ddb159b59d8f32
Thanh09Junior <148412524+Thanh09Junior@users.noreply.github.com>
2024-06-08 11:33:51 +0700

    Add files via upload

---
+++ CodeThucHanhLab1.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+/*Bai 1:
+int main()
+{
+	cout << "Hello, World!" << endl;
+	cout << "Hello, World! \n";
+	system("pause"); //Ngưng chương trình tạm thời (bấm nút bất kì để tiếp tục)
+	return 0;
+}
+*/
+
+/*Bai 2:
+int main()
+{
+	cout << "***************************" << endl;
+	cout << "*	Bai thuc hanh 1	  *" << endl;
+	cout << "*	Mon: CSLT	  *" << endl;
+	cout << "*	MSSV: 2351050164  *" << endl;
+	cout << "***************************" << endl;
+
+	system("pause"); 
+	return 0;
+}
+*/
+
+/*Bai 3:
+int main()
+{
+	cout << "  /////" << endl;
+	cout << " +-----+" << endl;
+	cout << "(| o o |)" << endl;
+	cout << " |  ^  |" << endl;
+	cout << " | ’-’ |" << endl;
+	cout << " +-----+" << endl;
+
+	system("pause");
+	return 0;
+}
+*/
+
+/*Bai 4:
+int main()
+{
+	cout << "  /\\" << endl;
+	cout << " /  \\" << endl;
+	cout << "+----+" << endl;
+	cout << "| .-.|" << endl;
+	cout << "| | ||" << endl;
+	cout << "+-+-++" << endl;
+
+	system("pause");
+	return 0;
+}
+*/
+
+/*Bai 5:
+int main()
+{
+	cout << "*********   ***     *       *" << endl;
+	cout << "*       *  *   *   ***     * *" << endl;
+	cout << "*       * *     * *****   *   *" << endl;
+	cout << "*       * *     *   *    *     *" << endl;
+	cout << "*       * *     *   *   *       *" << endl;
+	cout << "*       * *     *   *    *     *" << endl;
+	cout << "*       * *     *   *     *   *" << endl;
+	cout << "*       *  *   *    *      * *" << endl;
+	cout << "*********   ***     *       *" << endl;
+
+
+	system("pause");
+	return 0;
+}
+
+*/
+
+//Bai 6:
+int main()
+{
+	string username; //Khai báo kiểu dữ liệu string có tên username
+	cout << "Xin chao, ten toi la Hoa!" << endl;
+	cout << "Ten ban la gi?"; cin >> username;
+	cout << "Chao ban, " << username << "!" << endl;
+	cin.ignore(); //Muốn dùng getline thì phải có cái này nếu trước đó có thao tác nhập xuất
+	cout << "Ten ban la gi? "; getline(cin, username);
+	cout << "Chao ban, " << username << "!" << endl;
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
